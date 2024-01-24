7ca63ff5b3667bddb2d75353f51d43dfc0be5380
Johnafriedman <john@innovationsforlearning.org>
2024-01-24 11:38:22 -0600

    two frames partially working; second frame bounds iffy

--- bin/www/js/humanEnergy.js
+++ bin/www/js/humanEnergy.js
@@ -22,83 +22,107 @@ async function humanEnergy() {
 
     const bounds = [
         {
-            t: 440,
-            b: 2410,
-            x: 1024,
+            t: 210,
+            b: 1284,
+            x: 477,
             d: 1,
             color: "#FF0000FF"
         },
         {
-            t: 536,
-            b: 1412,
-            x: 3244,
+            t: 270,
+            b: 714,
+            x: 1624,
             d: -1,
             color: "#FFFFFFFF"
         }
     ];
 
-    let scaledBounds = [{},{}];
-
-    let scale = 1;
-
-    let srcCtx = null;
-    let destCtx = null;
-
-    let pixelImg = null;
-
-    let position = [];
+    let scaledBounds = [{}, {}];
 
     let width, height, figureHeight;
 
-    async function initializeImage() {
+    const IMAGE_PATH = "./assets/he/"
+    const URL = [
+        "humanEnergy1.jpg",
+        "humanEnergy2.jpg"
+    ];
 
-        let elem = document.getElementById("source");
-        srcCtx = elem.getContext('2d', {willReadFrequently: true});
-        elem = document.getElementById("destination");
-        destCtx = elem.getContext('2d');
 
-        const IMAGE_URL = "./assets/he/humanEnergy1.jpg";
-        let img = await loadImage(IMAGE_URL);
+    let frame = [];
 
-        scale = Math.min(innerWidth / img.width, window.innerHeight / img.height);
+    document.body.innerHTML = '';
 
-        width = Math.floor(img.width * scale);
-        height = Math.floor(img.height * scale);
-
-        srcCtx.canvas.width = width;
-        srcCtx.canvas.height = height;
-        destCtx.canvas.width = width;
-        destCtx.canvas.height = height;
-
-        for (let b = 0; b < bounds.length; b++) {
-            scaledBounds[b].t = Math.floor(bounds[b].t * scale);
-            scaledBounds[b].b = Math.floor(bounds[b].b * scale);
-            scaledBounds[b].x = Math.floor(bounds[b].x * scale);
-            scaledBounds[b].h = scaledBounds[b].b - scaledBounds[b].t;
-        }
-
-        const PIXEL_URL = "./assets/he/pixel.png";
-
-        pixelImg = await loadImage(PIXEL_URL);
-
-        srcCtx.drawImage(img, 0, 0, width, height);
-
-        figureHeight = Math.max(scaledBounds[LEFT].h, scaledBounds[LEFT].h)
-        position[LEFT] = new Array(figureHeight);
-        position[RIGHT] = new Array(figureHeight);
+    function visibleFrame(f, v) {
+        frame[f].src.elem.style.visibility = v ? 'visible' : 'hidden';
     }
 
-    function otherSide(side){
+
+    async function initializeImage() {
+
+        const visibility = ["visible", "hidden"];
+        for (let f = 0; f < URL.length; f++) {
+            let srcElem = document.createElement("canvas");
+            let isVisible = visibility[f];
+            srcElem.id = `source-${f}`;
+            srcElem.style = `z-index:1; visibility:${isVisible}`;
+            let srcCtx = srcElem.getContext('2d', {willReadFrequently: true});
+            document.body.appendChild(srcElem);
+            let dstElem = document.createElement("canvas");
+            dstElem.id = `destination-${f}`;
+            dstElem.style = `z-index:10; visibility:${isVisible}`;
+            let dstCtx = dstElem.getContext('2d');
+            document.body.appendChild(dstElem);
+
+            let img = await loadImage(`${IMAGE_PATH}${URL[f]}`);
+            let scale = Math.min(innerWidth / img.width, window.innerHeight / img.height);
+
+            for (let b = 0; b < bounds.length; b++) {
+                scaledBounds[b].t = Math.floor(bounds[b].t * scale);
+                scaledBounds[b].b = Math.floor(bounds[b].b * scale);
+                scaledBounds[b].x = Math.floor(bounds[b].x * scale);
+                scaledBounds[b].h = scaledBounds[b].b - scaledBounds[b].t;
+            }
+
+            figureHeight = Math.max(scaledBounds[LEFT].h, scaledBounds[LEFT].h);
+            width = Math.floor(img.width * scale);
+            height = Math.floor(img.height * scale);
+
+            srcCtx.canvas.width = width;
+            srcCtx.canvas.height = height;
+            dstCtx.canvas.width = width;
+            dstCtx.canvas.height = height;
+
+            srcCtx.drawImage(img, 0, 0, width, height);
+
+            let position = [];
+            position[LEFT] = new Array(figureHeight);
+            position[RIGHT] = new Array(figureHeight);
+            frame[f] = {
+                src: {
+                    elem: srcElem,
+                    ctx: srcCtx
+                },
+                dst: {
+                    elem: dstElem,
+                    ctx: dstCtx
+                }, position
+            };
+        }
+    }
+
+    function otherSide(side) {
         return side === LEFT ? RIGHT : LEFT;
     }
 
-    function skipColor(x, y, side, isForeground) {
+    function skipColor(f, x, y, side, isForeground) {
         const THRESHOLD = 24;
 
         let color, imageData;
         let bound = scaledBounds[otherSide(side)].x;
         do {
-            imageData = srcCtx.getImageData(x, y, 1, 1);
+            imageData = frame[f].src.ctx.getImageData(x, y, 1, 1);
+            /*            frame[f].dst.ctx.fillStyle = "#FF0000FF";
+                        frame[f].dst.ctx.fillRect(x, y, 1, 1);*/
             x += bounds[side].d;
             color = (imageData.data[RED] > THRESHOLD) || (imageData.data[GREEN] > THRESHOLD) || (imageData.data[BLUE] > THRESHOLD);
         } while ((color == isForeground) && (side === LEFT ? x < bound : x > bound));
@@ -108,83 +132,106 @@ async function humanEnergy() {
 
     function initializeAnimation() {
 
-        for (let side = LEFT; side <= RIGHT; side++) {
-            let y = scaledBounds[side].t;
-            let x = scaledBounds[side].x;
-            for (let i = 0; i < figureHeight; i++) {
+        for (let f = 0; f < frame.length; f++) {
 
-                let x1 = skipColor(x, y + i, side, false);
-                let x2 = skipColor(x1, y + i, side, true);
-                position[side][i] = {start: x1, end: x2, current: x2, delta: Math.sign(x2-x1) * DELTA, mode: DRAW_MODE};
+            for (let side = LEFT; side <= RIGHT; side++) {
+                let y = scaledBounds[side].t;
+                let x = scaledBounds[side].x;
+                for (let i = 0; i < figureHeight; i++) {
+
+                    let x1 = skipColor(f, x, y + i, side, false);
+                    let x2 = skipColor(f, x1, y + i, side, true);
+                    frame[f].position[side][i] = {
+                        start: x1,
+                        end: x2,
+                        current: x2,
+                        delta: Math.sign(x2 - x1) * DELTA,
+                        mode: DRAW_MODE
+                    };
+                }
             }
         }
+
     }
 
     function animate() {
-        const DELAY = 80, LEFT_DOTS = 100, RIGHT_DOTS = 50;
+        const DOT_DELAY = 80, FRAME_DELAY_MIN = 3, FRAME_DELAY_MAX = 20, LEFT_DOTS = 100, RIGHT_DOTS = 50;
 
-        destCtx.globalAlpha = 1.0;
-        destCtx.globalCompositeOperation = "source-over";
-        // Shadow
-        // destCtx.shadowColor = "#FFFF0010";
-        destCtx.shadowOffsetX = 20;
-        destCtx.shadowOffsetY = 20;
-        // destCtx.shadowBlur = 15;
+        for (let f = 0; f < frame.length; f++) {
+            frame[f].dst.ctx.globalAlpha = 1.0;
+            frame[f].dst.ctx.globalCompositeOperation = "source-over";
+            // Shadow
+            // frame[f].dst.ctx.shadowColor = "#FFFF0010";
+            frame[f].dst.ctx.shadowOffsetX = 20;
+            frame[f].dst.ctx.shadowOffsetY = 20;
+            // frame[f].dst.ctx.shadowBlur = 15;
+        }
 
-            let id;
 
-        function drawADot(p, y, side){
+        let id;
+
+        function drawADot(f, p, y, side) {
 
             if (p.mode === DRAW_MODE) {
-                destCtx.fillRect(p.current, scaledBounds[side].t + y, pWIDTH, pHEIGHT);
+                frame[f].dst.ctx.fillRect(p.current, scaledBounds[side].t + y, pWIDTH, pHEIGHT);
             } else {
-                destCtx.clearRect(p.current, scaledBounds[side].t + y, pWIDTH, pHEIGHT)
+                frame[f].dst.ctx.clearRect(p.current, scaledBounds[side].t + y, pWIDTH, pHEIGHT)
             }
 
             p.current += p.delta;
-            if((p.current >= position[RIGHT][y].end) || (p.current <= position[LEFT][y].end)) {
+            if ((p.current >= frame[f].position[RIGHT][y].end) || (p.current <= frame[f].position[LEFT][y].end)) {
                 p.mode = !p.mode;
                 p.delta *= -1;
             }
             return p;
         }
 
+        function toggleFrame() {
+            if (frame[0].src.elem.style.visibility === 'visible') {
+                frame[0].src.elem.style.visibility = 'hidden';
+                frame[1].src.elem.style.visibility = 'visible';
+            } else {
+                frame[1].src.elem.style.visibility = 'hidden';
+                frame[0].src.elem.style.visibility = 'visible';
+            }
+            ;
+        }
+
+        function newNextFrame(){
+            return Math.floor(Math.random()*FRAME_DELAY_MAX)+FRAME_DELAY_MIN;
+        }
+
+        let nextFrame = newNextFrame();
         function drawDots() {
 
-            destCtx.fillStyle = bounds[LEFT].color;
-            for (let i=0; i < LEFT_DOTS; i++) {
+            for (let f = 0; f < frame.length; f++) {
 
-                let y = Math.floor(Math.random() * (scaledBounds[LEFT].h/(pHEIGHT*2)))*pHEIGHT*2;
-                let p = position[LEFT][y];
-                p = drawADot(p, y, LEFT);
+                frame[f].dst.ctx.fillStyle = bounds[LEFT].color;
+                for (let i = 0; i < LEFT_DOTS; i++) {
+
+                    let y = Math.floor(Math.random() * (scaledBounds[LEFT].h / (pHEIGHT * 2))) * pHEIGHT * 2;
+                    let p = frame[f].position[LEFT][y];
+                    p = drawADot(f, p, y, LEFT);
+                }
+
+                frame[f].dst.ctx.fillStyle = bounds[RIGHT].color;
+                for (let i = 0; i < RIGHT_DOTS; i++) {
+
+                    let y = Math.floor(Math.random() * (scaledBounds[RIGHT].h / (pHEIGHT * 2))) * pHEIGHT * 2 + pHEIGHT;
+                    let p = frame[f].position[RIGHT][y];
+                    p = drawADot(f, p, y, RIGHT);
+                }
+
             }
-
-            destCtx.fillStyle = bounds[RIGHT].color;
-            for (let i=0; i < RIGHT_DOTS; i++) {
-
-                let y = Math.floor(Math.random() * (scaledBounds[RIGHT].h/(pHEIGHT*2))) *pHEIGHT *2 +pHEIGHT;
-                let p = position[RIGHT][y];
-                p = drawADot(p, y, RIGHT);
+            if(--nextFrame<0){
+                nextFrame = newNextFrame();
+                toggleFrame();
             }
-
         }
 
-        id = setInterval(drawDots, DELAY)
+        id = setInterval(drawDots, DOT_DELAY);
 
-    }
 
-    function getImageContext(url) {
-
-        let img = null;
-        loadImage(url).then((value => img = value));
-        if (typeof window.createImageBitmap === 'function') {
-            createImageBitmap(img).then((value => img = value));
-        }
-        const ctx = get2DContext(img.width, img.height);
-
-        ctx.drawImage(img, 0, 0);
-
-        return ctx;
     }
 
 // some helpers
@@ -197,13 +244,6 @@ async function humanEnergy() {
             img.onload = e => res(img);
             img.onerror = rej;
         });
-    }
-
-    function get2DContext(width = 300, height = 150) {
-        return Object.assign(
-            document.createElement('canvas'),
-            {width: width, height: height}
-        ).getContext('2d');
     }
 
     await initializeImage();
